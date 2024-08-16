a3173b70a621b3ce132f5c0229a2016cca2ea2dc
ItsElizabethNkwe <163154686+ItsElizabethNkwe@users.noreply.github.com>
2024-08-16 16:00:30 +0200

    Updated the small element in Javascript

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function updateTime() {
 
   losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
   losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "h:mm:ss [<small>A</small>]"
+    "h:mm:ss [<small>AM</small>]"
   );
 }
 
@@ -18,7 +18,7 @@ function updateTime() {
   let parisTime = moment().tz("Europe/Paris");
 
   parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
-  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>A</small>]"
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>AM</small>]"
 
   );
  }
